11e10b1a48d0908dab6cef57fc16b4c5867a7420
yanndias <yann.dias22@gmail.com>
2019-04-24 00:19:20 -0300

    Nome da janela atualizado

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(settings) -- essa function só funciona dentro de conf.lua
     settings.window.width = 800
     settings.window.height = 600
-    settings.title = 'Attack on Zoombi'
+    settings.title = 'Ultimato Zumbi: Exterminio'
     settings.console = true
     _G.Debug = true
 end
