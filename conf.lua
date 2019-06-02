9d4f58cd676db79ad580c658a4e84029d9f83e48
YANN <yann@aluno.puc-rio.br>
2019-06-02 15:30:40 -0300

    Removed all old root files

--- conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(settings) -- essa function só funciona dentro de conf.lua
-    settings.window.width = 800
-    settings.window.height = 600
-    settings.title = 'Ultimato Zumbi: Exterminio'
-    settings.console = true
-    _G.Debug = true
-end
